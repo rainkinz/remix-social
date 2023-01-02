62b3e84d4a50edfd668903d347c819207fb6c6c0
Brendan Grainger <brendan.grainger@gmail.com>
2023-01-02 17:03:40 +1300

    added UserForm

---
+++ app/routes/signup.tsx
@@ -0,0 +1,10 @@
+import { UserForm } from '~/components/UserForm'
+
+export default function SignUpPage() {
+  return (
+    <div>
+      <h1 className="mb-8 text-xl text-slate-800">Sign Up</h1>
+      <UserForm />
+    </div>
+  )
+}
